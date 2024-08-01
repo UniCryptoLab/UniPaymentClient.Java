5814b1456682fc9bef55053867d1a42bbe3d38d4
Shameem Peerbuccoss <shameem.peerbuccoss@gmail.com>
2024-08-01 22:01:18 +0400

    Update Transaction Models and SDK Example

--- unipayment-java-sdk/src/main/java/io/unipayment/sdk/model/QueryPaymentRequest.java
+++ unipayment-java-sdk/src/main/java/io/unipayment/sdk/model/QueryPaymentRequest.java
@@ -1,11 +1,24 @@
 package io.unipayment.sdk.model;
 
+import io.unipayment.sdk.model.enums.PaymentStatus;
+import io.unipayment.sdk.model.enums.TransferMethod;
+import lombok.Getter;
+
 import java.util.TreeMap;
 
+@Getter
 public class QueryPaymentRequest extends TreeMap<String, Object> {
     private int pageNo = 1;
     private int pageSize = 10;
     private Boolean isAsc;
+    private String fromAccountId;
+    private String toAccountId;
+    private TransferMethod transferMethod;
+    private String paymentMethodId;
+    private String cryptoAddress;
+    private String bankAccount;
+    private String assetType;
+    private PaymentStatus status;
 
     public QueryPaymentRequest() {
         put("page_no", pageNo);
@@ -22,12 +35,48 @@ public class QueryPaymentRequest extends TreeMap<String, Object> {
         put("page_size", pageSize);
     }
 
-    public Boolean isAsc() {
-        return isAsc;
-    }
-
     public void setIsAsc(Boolean asc) {
         isAsc = asc;
         put("is_asc", isAsc);
+    }
+
+    public void setFromAccountId(String fromAccountId) {
+        this.fromAccountId = fromAccountId;
+        put("from_account_id", fromAccountId);
+    }
+
+    public void setToAccountId(String toAccountId) {
+        this.toAccountId = toAccountId;
+        put("to_account_id", toAccountId);
+    }
+
+    public void setTransferMethod(TransferMethod transferMethod) {
+        this.transferMethod = transferMethod;
+        put("transfer_method", transferMethod.name());
+    }
+
+    public void setPaymentMethodId(String paymentMethodId) {
+        this.paymentMethodId = paymentMethodId;
+        put("payment_method_id", paymentMethodId);
+    }
+
+    public void setCryptoAddress(String cryptoAddress) {
+        this.cryptoAddress = cryptoAddress;
+        put("crypto_address", cryptoAddress);
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+        put("bank_account", bankAccount);
+    }
+
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+        put("asset_type", assetType);
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+        put("status", status.name());
     }
 }
